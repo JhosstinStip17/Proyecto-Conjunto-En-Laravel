a219f50fcc5b03d381cfbd6efcd17e6a32922ba2
JhosstinStip17 <jhosstin123vargas@gmail.com>
2025-01-29 14:41:37 -0500

    Segundo commit

---
+++ app/Http/Controllers/SancionesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sanciones;
+use Illuminate\Http\Request;
+
+class SancionesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sanciones  $sanciones
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sanciones $sanciones)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sanciones  $sanciones
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sanciones $sanciones)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sanciones  $sanciones
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sanciones $sanciones)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sanciones  $sanciones
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sanciones $sanciones)
+    {
+        //
+    }
+}
