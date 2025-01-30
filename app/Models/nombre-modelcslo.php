2fea21c8cfe8d3d4c37aa3e04f28eb541a8b2c9f
Stip Vargas <141756797+JhosstinStip17@users.noreply.github.com>
2025-01-30 17:53:21 +0000

    commit

---
+++ app/Models/nombre-modelcslo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class nombre-modelcslo extends Model
+{
+    use HasFactory;
+}
